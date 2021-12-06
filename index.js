43a29ed0479a480febd3725af0d2f9dbac3387af
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2021-12-06 16:25:08 +0000

    Complete all of Task 2

--- index.js
+++ index.js
@@ -19,7 +19,35 @@ let myCollection = [
 console.log(myCollection);
 
 function describeItem(object) {
-  console.log(object.count);
+  //console.log(object.count);
+
+//Plan for 2c
+//if theres one item (count property)
+  //log I have a name. Here's what I like about it: whatILike
+//if theres more than one
+  //I have count names. Here's what I like about them: whatILike
+
+  if (object.count === 1) {
+    console.log(`I have a ${object.name}. Here's what I like about it: ${ object.whatILike }`);
+  } else {
+    console.log(`I have ${object.count}, ${object.name}. Here's what I like about them: ${ object.whatILike }`);
+  }
+
 }
 
-describeItem();
+//describeItem(myCollection[0]);
+
+
+//Task 2d
+//make a describeCollection function
+//input parameter is an array
+//loop through the input array
+//for each item call describeItem function
+
+function describeCollection(collection) {
+  
+  collection.forEach(describeItem);
+
+}
+
+describeCollection(myCollection);
