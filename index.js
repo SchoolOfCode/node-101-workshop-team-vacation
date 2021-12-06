cba7a1c400fe2bffc5a0ae82ecd9235a37c5e412
Cintia Siqueira <cii_3@hotmail.com>
2021-12-06 16:07:41 +0000

    first part 2C

---
+++ index.js
@@ -0,0 +1,25 @@
+let myCollection = [
+  {
+    name: "School of Code mug",
+    count: 1,
+    whatILike: "It has my cute pixel character on it!",
+  },
+  {
+    name: "School of Code hat",
+    count: 2,
+    whatILike: "An often overlooked fashion accessory",
+  },
+  {
+    name: "School of Code pillow",
+    count: 1,
+    whatILike: "Eat. Sleep. Code. Repeat :)",
+  },
+];
+
+console.log(myCollection);
+
+function describeItem(object) {
+  console.log(object.count);
+}
+
+describeItem();
